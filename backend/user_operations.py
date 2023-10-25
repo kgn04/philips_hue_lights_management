f6f685de8ac0cceed3babbe054c0b662013f9c8b
Kacper Gaudyn <kacper.gaudyn@pwr.edu.pl>
2023-10-25 12:28:24 +0200

    user operations implemented.

---
+++ backend/user_operations.py
@@ -0,0 +1,47 @@
+import db_management
+from re import match
+from sqlite3 import OperationalError
+
+OPERATION_SUCCESSFUL = 0
+EMAIL_ALREADY_EXISTS = 1
+DIFFERENT_PASSWORDS = 2
+INCORRECT_EMAIL = 3
+INCORRECT_PASSWORD = 4
+INCORRECT_USERNAME = 5
+
+
+def validate_email(email: str) -> bool:
+    email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return bool(match(email_pattern, email))
+
+
+def validate_password(password: str) -> bool:
+    if len(password) < 8 or \
+            not any(char.isupper() for char in password) or \
+            not any(char.isnumeric() for char in password):
+        return False
+    return True
+
+
+def register(email: str, username: str, password1: str, password2: str) -> int:
+    if db_management.select('Uzytkownicy', 'Email', ('Email', email)):
+        return EMAIL_ALREADY_EXISTS
+    elif password1 != password2:
+        return DIFFERENT_PASSWORDS
+    elif not validate_email(email):
+        return INCORRECT_EMAIL
+    elif not validate_password(password1):
+        return INCORRECT_PASSWORD
+    elif not username:
+        return INCORRECT_USERNAME
+    db_management.insert('Uzytkownicy', (email, username, password1))
+    return OPERATION_SUCCESSFUL
+
+
+def login(email: str, password: str) -> int:
+    try:
+        if db_management.select('Uzytkownicy', 'Haslo', ('Email', email)) != password:
+            return INCORRECT_PASSWORD
+    except OperationalError:
+        return INCORRECT_EMAIL
+    return OPERATION_SUCCESSFUL
