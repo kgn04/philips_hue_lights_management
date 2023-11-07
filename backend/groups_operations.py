a0c73385187256f5d5c28668ef473623c947431c
Kacper Gaudyn <kacper.gaudyn@pwr.edu.pl>
2023-11-07 12:05:26 +0100

    lights count in hub, group operations, resizable turned off.

--- backend/groups_operations.py
+++ backend/groups_operations.py
@@ -3,8 +3,9 @@ from sqlite3 import OperationalError
 
 SUCCESSFUL_OPERATION = 0
 GROUP_NAME_ALREADY_USED = 1
-LIGHT_ALREADY_IN_GROUP = 2
-LIGHT_NOT_IN_GROUP = 3
+GROUP_DOES_NOT_EXIST = 2
+LIGHT_ALREADY_IN_GROUP = 3
+LIGHT_NOT_IN_GROUP = 4
 
 
 current_hub_login: str = ''
@@ -12,21 +13,37 @@ current_hub_ip: str = ''
 request_prefix: str = ''
 
 
-def create(group_name: str, lights_ids: list[int]) -> int:
+def create(group_name: str) -> int:
     if group_name in db_management.select_all('Grupy', 'NazwaGr'):
         return GROUP_NAME_ALREADY_USED
     try:
-        id = max(db_management.select_all('Grupy', 'IdGr')) + 1
+        group_id = max(db_management.select_all('Grupy', 'IdGr')) + 1
     except OperationalError:
-        id = 1
+        group_id = 1
+    db_management.insert('Grupy', (group_id, group_name))
+    return SUCCESSFUL_OPERATION
+
+
+def delete(group_id: int) -> int:
+    if group_id not in db_management.select_all('Grupy', 'IdGr'):
+        return GROUP_DOES_NOT_EXIST
+    db_management.delete('Grupy', ('IdGr', group_id))
+    return SUCCESSFUL_OPERATION
 
 
 def add_to_group(group_id: int, light_id: int) -> int:
-    pass
+    if light_id in db_management.select('Przypisania', 'IdK', ('IdGr', group_id)):
+        return LIGHT_ALREADY_IN_GROUP
+    db_management.insert('Przypisania', (group_id, light_id))
+    return SUCCESSFUL_OPERATION
 
 
 def delete_from_group(group_id: int, light_id: int) -> int:
-    pass
+    # TODO Lepiej nie uzywac jeszcze lmao
+    if light_id not in db_management.select('Przypisania', 'IdK', ('IdGr', group_id)):
+        return LIGHT_NOT_IN_GROUP
+    db_management.delete('Przypisania', ('IdK', light_id))
+    return SUCCESSFUL_OPERATION
 
 
 def __change_current_hub_2(mac_address: str) -> None:
